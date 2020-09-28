6d0ff6e0b1e7223d40f87df5b6380f4fad57af35
Kee Tam Hoang <hoangvh1@ghtk.vn>
2020-09-28 20:31:02 +0700

    update format

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -128,9 +128,12 @@ class AuthController extends BaseController
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60
+            'success' => true,
+            'data' => [
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => auth()->factory()->getTTL() * 60
+            ]
         ]);
     }
 }
