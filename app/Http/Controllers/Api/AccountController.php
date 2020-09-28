885c7b22e168220c8cdf9d7ffeddacad57bf3eb1
Kee <vohuyhoang112@gmail.com>
2020-09-28 21:46:53 +0700

    update

--- app/Http/Controllers/Api/AccountController.php
+++ app/Http/Controllers/Api/AccountController.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Controller;
 class AccountController extends BaseController
 {
     public function analytics() {
-        $account = Account::where('shop_id', $this->shopNow)->where('id', auth()->user()->id)->first();
+        $account = Account::select('id', 'fullname', 'username', 'balance', 'email')->where('shop_id', $this->shopNow)->where('id', auth()->user()->id)->first();
 
         if (empty($account)) {
             return response(Utils::FailedResponse('Không tìm thấy tài khoản này trên trang web của bạn'));
@@ -21,12 +21,12 @@ class AccountController extends BaseController
         $thisMonth = Carbon::now()->startOfMonth()->toDateTimeString();
 
         $amountThisMonth = Transaction::selectRaw('sum(amount) as amount_this_month')->where('shop_id', $this->shopNow)->where('from_account_id', auth()->user()->id)
-            ->where('type', Transaction::TYPE_TOP_UP)->where('created_at', '>=', $thisMonth)->first();
+            ->where('type', Transaction::TYPE_TOP_UP)->where('created_at', '>=', $thisMonth)->where('status', Transaction::STATUS_SUCCESS)->first();
 
         $account->amount_this_month = !empty($amountThisMonth->amount_this_month) ? $amountThisMonth->amount_this_month : 0;
 
         $amountTotal = Transaction::selectRaw('sum(amount) as amount_total')->where('shop_id', $this->shopNow)->where('from_account_id', auth()->user()->id)
-            ->where('type', Transaction::TYPE_TOP_UP)->first();
+            ->where('type', Transaction::TYPE_TOP_UP)->where('status', Transaction::STATUS_SUCCESS)->first();
 
         $account->amount_total = !empty($amountTotal->amount_total) ? $amountTotal->amount_total : 0;
 
