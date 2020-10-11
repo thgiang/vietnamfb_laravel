12336292f4996ca7571513e83042732760447ee7
Kee <vohuyhoang112@gmail.com>
2020-10-11 13:11:22 +0700

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,5 +14,8 @@ class Post extends Model
         return $this->belongsTo(Account::class)->select('id', 'fullname', 'username');
     }
 
-
+    public function getImageAttribute()
+    {
+        return env('APP_URL') . $this->attributes['image'];
+    }
 }
